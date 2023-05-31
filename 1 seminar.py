118c26b2c7a54faaabd06a0c0b654a6fe96858d3
Drakula <vbuzinskij@gmail.com>
2023-05-31 23:53:11 +0300

    phones

--- 1 seminar.py
+++ 1 seminar.py
@@ -1,55 +1,121 @@
-# Задача 34:  Винни-Пух попросил Вас посмотреть, есть ли в его стихах ритм. Поскольку разобраться в его кричалках не настолько 
-# просто, насколько легко он их придумывает, Вам стоит написать программу. Винни-Пух считает, что ритм есть, если число слогов 
-# (т.е. число гласных букв) в каждой фразе стихотворения одинаковое. Фраза может состоять из одного слова, если в фразе несколько 
-# слов, то они разделяются дефисами. Фразы отделяются друг от друга пробелами. Стихотворение  Винни-Пух вбивает в программу с 
-# клавиатуры. В ответе напишите “Парам пам-пам”, если с ритмом все в порядке и “Пам парам”, если с ритмом все не в порядке
-print("Задача 34")
-input34=input("Введите фразу: ")
-list_0=list(input34)
-list_1=list(filter(lambda x: (x.isalpha()) or x=="-" or x==" " ,list_0))
-if len(list_0)==len(list_1):
-    list_1=input34.split(" ")
-    list_1=list(map(lambda x: list(x),list_1))
-    gl={"а":1,"у":1,"о":1,"ы":1,"и":1,"э":1,"я":1,"ю":1,"ё":1,"е":1,"А":1,"У":1,"О":1,"Ы":1,"и":1,"Э":1,"Я":1,"Ю":1,"Ё":1,"е":1}
-    list_0=list()
-    for i in range(len(list_1)):
-        s=0
-        for j in list_1[i]:
-            if j in gl:
-                s=s+gl[j]
-        list_0.append(s)
-    res34=True
-    for i in range(len(list_0)-1):
-        if list_0[i]!=list_0[i+1]:
-            res34=False
-    if res34==True: print("Парам пам-пам") 
-    else: print("Пам парам")        
-else: print("Ошибка")
+# 1. Открыть файл 
+# 2. Сохранить файл
+# 3. Показать все контакты
+# 4. Добавить контакт
+# 5. Найти контакт
+# 6. Изменить контакт
+# 7. Удалить контакт
+# 8. Выход
+import os.path
+phones=[]
 
-# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), которая принимает в качестве аргумента функцию,
-# вычисляющую элемент по номеру строки и столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,
-# которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). Примечание: бинарной
-# операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
+# 1. Открыть файл 
+def open_file (file_name):
+    file =os.path.exists(file_name)
+    if file:
+        global phones
+        f = open(file_name, 'r')
+        phones=(f.readlines())
+        phones=list(map(lambda x: x[:-1] ,phones ))                 
+        f.close()
+        return True
+    else: return False
+
+# 2. Сохранить файл
+def save_file (file_name,lis=[]):
+    file =os.path.exists(file_name)
+    if file:
+        lis=list(map(lambda x:x+"\n",lis))
+        lis=''.join(lis)
+        f = open(file_name, 'w')
+        f.write(lis)
+        f.close()
+        return True
+    else: return False
+
+# 3. Показать все контакты
+def print_file ():
+    f=list(map(lambda x : x.replace(',',' '),phones))
+    for i, val in enumerate(f, start=1):
+        print(f'№ {i}    {val}')
+
+# 4. Добавить контакт
+def add_contact(name,phone,comm):
+    contact=(f'{name},{phone},{comm}')
+    phones.append(contact)
+
+# 5. Найти контакт
+def search_contact (name):
+    res=list()
+    for i in range(len(phones)):
+        con=phones[i].split(",")      
+        if name in con[0]:
+            res.append(f'№{i+1}    {phones[i]}')
+    f=list(map(lambda x : x.replace(',',' '),res))  
+    for i in f:print(i)
+    return res
+
+# 6. Изменить контакт
+def edit_contact(number,new_name,new_phone,new_comm):
+    if (int(number)<=len(phones)):
+        phones[int(number)-1]=(f'{new_name},{new_phone},{new_comm}')
+        return True
+    else:return False
+  
+# 7. Удалить контакт
+def pop_contact(number):
+    if (int(number)<=len(phones)):
+        phones.pop(int(number)-1)
+        return True
+    else: return False
+
+# 8. Выход
 
 
-
-def print_operation_table(operation, num_rows=6, num_сolumns=6):
-    array=[[operation(i,j) for i in range(1,num_rows+1)] for j in range(1, num_сolumns+1)]
-    for i in array:
-        print(" ".join(map(str,i)))
-print("Задача 36")
-
-rows=input("Введите количество строк: ")
-while not(rows.isdigit()):
-    rows=input("Введите количество строк: ")
-columns=input("Введите количество столбцов: ")
-while not(columns.isdigit()):
-    columns=input("Введите количество столбцов: ")
-print_operation_table(lambda x,y: x*y,int(rows),int(columns))
-
-
-
-
+start=True
+while start:
+    status=input("Введите '1', чтобы открыть файл или '0'чтобы выйти из программы: ")
+    if status=="1":
+        file_name=input("введите имя файла: ")
+        if open_file (file_name):
+            print("Файл открыт")
+            start1=True
+            while start1:
+                status1=input("Введите '1', чтобы сохранить файл \nВведите '2', чтобы показать все контакты\nВведите '3', чтобы добавить контакт\nВведите '4', чтобы найти контакт\nВведите '5', чтобы изменить контакт\nВведите '6', чтобы удалить контакт\nВведите '7', чтобы вернуться в предыдущее меню:  ")
+                if status1=="1":
+                    print("Сохранение файла")
+                    save_file (file_name,phones)
+                elif status1=="2":
+                    print("Все контакты")
+                    print_file ()
+                elif status1=="3":
+                    print("Добавление контакта")
+                    name=input("Введите ФИО:  ")
+                    phone=input("Введите телефон:  ")
+                    comm=input("Введите комментарий:  ")
+                    add_contact(name,phone,comm)
+                elif status1=="4":
+                    print("Поиск контакта")
+                    name=input("Введите ФИО полостью или частично:  ")
+                    search_contact (name)
+                elif status1=="5":
+                    print("Изменение контакта")
+                    number=input("Введите порядковый номер контакта:  ")
+                    new_name=input("Введите ФИО:  ")
+                    new_phone=input("Введите телефон:  ")
+                    new_comm=input("Введите комментарий:  ")
+                    if edit_contact(number,new_name,new_phone,new_comm): print("Изменение внесены")
+                    else: print("Ошибка")    
+                elif status1=="6":
+                    print("Удаление контакта")
+                    number=input("Введите порядковый номер контакта:  ")
+                    if pop_contact(number): print("контакт удален")
+                    else: print("Ошибка")
+                elif status1=="7":
+                    print("Возврат к предыдущему меню")
+                    start1=False
+        else: print("Файла не существует")
+    elif status=="0": start=False
 
 
 
