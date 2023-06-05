959bf0c685f4216b121e97c1dcd1f2209da1026c
Drakula <vbuzinskij@gmail.com>
2023-06-05 19:34:03 +0300

    2

--- controller.py
+++ controller.py
@@ -1,16 +1,19 @@
 import view
 import model
+my_pb=model.PhoneBook()
+
 def start1():
     while True:
         choice=view.main_menu1()
 
 
-
         match choice:
             case 1:
-                
-                if model.open_file(): start2()
-          
+                my_pb._file_name=view.read_name_file()
+                if my_pb.open_file(): 
+                    view.done(True)
+                    start2()
+                else: view.done(False)
             case 2:
                 break
 
@@ -19,19 +22,25 @@ def start2():
         choice=view.main_menu2()
 
 
-
         match choice:
             case 1:
-                model.save_file()
+                if my_pb.save_file(): view.done(True)
+                else: view.done(False)
             case 2:
-                model.print_file ()
+                f=list(my_pb.print_file (my_pb.phones))
+                view.print_list(f)
             case 3:
-                model.add_contact()
+                my_pb.add_contact(list(view.input_data_contact()))
+                view.done(True)
             case 4:
-                model.search_contact ()
+                if my_pb.search_contact (view.search_contact()): view.done(True)
+                else: view.done(False)
+
             case 5:
-                model.edit_contact()
+                if my_pb.edit_contact(view.orger_number(),list(view.input_data_contact())): view.done(True)
+                else: view.done(False)
             case 6:
-                model.pop_contact()  
+                if my_pb.pop_contact(view.orger_number()): view.done(True)
+                else: view.done(False)
             case 7:
                 break
